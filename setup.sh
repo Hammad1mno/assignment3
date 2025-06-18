e9a0b18c1bc35860b7a08e9dddf5016cafb1f2fb
l1s22bsse0194 <l1s22bsse0194@ucp.edu.pk>
2025-06-18 13:26:03 +0500

    my initial commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+git init
+git branch -m develop 
+mkdir ./src ./styles ./asserts ./bin  ./node_modules 
+touch ./.gitignore ./src/index.html ./styles/index.css
+
+git config --local user.name "l1s22bsse0194"
+git config --local user.email "l1s22bsse0194@ucp.edu.pk"
+git config --local core.editor "notepad"
+git remote add origin https://github.com/Hammad1mno/assignment3.git
